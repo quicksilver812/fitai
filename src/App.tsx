4ffd6a52f04d03b4a849c49ad2ce600bb81f7752
quicksilver812 <allendsouza812@gmail.com>
2024-11-25 17:39:18 +0530

    fixed page title

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const App: React.FC<React.PropsWithChildren<AppProps>> = ({
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>FitAi</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Create your own AI chat bot" />
       </Head>
