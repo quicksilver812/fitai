2f0f3b7949c3b931cd74f4f4e94199c19c0b5214
quicksilver812 <allendsouza812@gmail.com>
2024-11-25 16:57:57 +0530

    updated Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,14 +1,12 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am FitAi</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      I'm here to help you stay fit, healthy, and on track with your workout goals. Whether you're looking for a custom workout plan, fitness tips, or just some motivation, I've got you covered. Let’s crush those goals together! 💪
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
+      <a className="underline" href="https://github.com/quicksilver812/fitai/blob/main/README.md">
+        Github
       </a>
     </p>
   </div>
